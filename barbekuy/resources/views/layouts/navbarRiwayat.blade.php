bbe6ab4df91ef0970c9d0573ee90f63d56aab998
liyakhoirunnisa <lykhoirunnisa@gmail.com>
2025-10-29 22:10:30 +0700

    tes

--- barbekuy/resources/views/layouts/navbarRiwayat.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="tabs">
-    <a href="/riwayatSemua" class="{{ request()->is('riwayatSemua') ? 'active' : '' }}">Semua</a>
-    <a href="/riwayatProses" class="{{ request()->is('riwayatProses') ? 'active' : '' }}">Sedang Proses</a>
-    <a href="/riwayatSelesai" class="{{ request()->is('riwayatSelesai') ? 'active' : '' }}">Selesai</a>
-    <a href="/riwayatBatal" class="{{ request()->is('riwayatBatal') ? 'active' : '' }}">Dibatalkan</a>
-</div>
